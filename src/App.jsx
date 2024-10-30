3c7be41230497816890d44a5207e1cf8a10a54a1
javaniecampbell <javanie.mcampbell@outlook.com>
2024-10-30 11:41:34 -0400

    style: adjust the styles to ensure rendered properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import './index.css';
 import './App.css'
 
 // Main entry file for a Next.js or Vite app that uses the NetworkDiagram component
 // import ReactDOM from 'react-dom';
 import NetworkDiagram from './NetworkDiagram'; // Import the NetworkDiagram component
-import './index.css';
 
 const SplashPage = ({ navigateToLanding }) => {
   return (
